ead604295d8bcacfb0ab2654f9f9e1e6b72daff3
whoissarisss <sarasramirezp45@gmail.com>
2026-03-10 12:10:51 -0500

    creacion de menu

---
+++ main.py
@@ -0,0 +1,7 @@
+print("BIENVENIDO A LA COFFEESHOP BY SARISSS")
+print("""
+        Menu de bebidas
+        1. Capuccino tradicional  $9000
+        2. Expresso doble         $8500
+        3. Latte matcha           $7500
+""")
